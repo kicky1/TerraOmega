e264e6bbaa8bfeac148376934f251fffa212fd30
kicky1 <krzysiekwicki1@gmail.com>
2023-03-15 22:08:45 +0100

    Change request

--- src/app/utils/actions/users.tsx
+++ src/app/utils/actions/users.tsx
@@ -1,7 +1,7 @@
 import api from '../api'
 
 export async function getUsersData() {
-    const { data } = await api.get('battle',{})
+    const { data } = await api.get('players',{})
     let players = data.players
     players = players.filter((obj: any) => obj.hasOwnProperty('username'));
     return players
