d3ca4a1f13170203fd3187731a9fcea0f0b81958
kicky1 <krzysiekwicki1@gmail.com>
2023-03-18 17:28:29 +0100

    Add package

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,4 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@vercel/edge-config"
+
 
 const supabaseUrl = 'https://vxbjqybxueympyxebcqs.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4YmpxeWJ4dWV5bXB5eGViY3FzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg1NzMxMTUsImV4cCI6MTk5NDE0OTExNX0.5uwwjOPL62Oq-1epq8m02sL2h-amwG4uf6Ga1xML1kE'
