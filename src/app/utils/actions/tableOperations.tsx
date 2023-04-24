7432ecc265777b83d673d462056b656bd47ef3c3
kicky1 <krzysiekwicki1@gmail.com>
2023-04-24 20:34:30 +0200

    Fix undefined values for stats

--- src/app/utils/actions/tableOperations.tsx
+++ src/app/utils/actions/tableOperations.tsx
@@ -1,10 +1,15 @@
 export function getStatValue(item: any, stat: string) {
-    const itemStat = item.stats?.[stat] || 0;
-    const armorStat = item.items.armor?.atributes?.[stat] || 0;
-    const avatarStat = item.items.avatar?.atributes?.[stat] || 0;
-    const shipStat = item.items.ship?.atributes?.[stat] || 0;
-    const specialStat = item.items.special?.atributes?.[stat] || 0;
-    const weaponStat = item.items.weapon?.atributes?.[stat] || 0;
-  
-    return itemStat + armorStat + avatarStat + shipStat + specialStat + weaponStat;
+    if(item.items){
+      const itemStat = item.stats?.[stat] || 0;
+      const armorStat = item.items.armor?.atributes?.[stat] || 0;
+      const avatarStat = item.items.avatar?.atributes?.[stat] || 0;
+      const shipStat = item.items.ship?.atributes?.[stat] || 0;
+      const specialStat = item.items.special?.atributes?.[stat] || 0;
+      const weaponStat = item.items.weapon?.atributes?.[stat] || 0;
+    
+      return itemStat + armorStat + avatarStat + shipStat + specialStat + weaponStat;
+    }else{
+      return 0
+    }
+
   }
